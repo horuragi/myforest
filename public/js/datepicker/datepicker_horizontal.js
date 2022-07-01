6366d184374b46ba824eeef66d9af90fd2c8aed1
zivi <frank910@naver.com>
2022-07-01 20:25:23 +0900

    주차예약 페이지 유효성 검사 및 선택 기능 구현

--- public/js/datepicker/datepicker_horizontal.js
+++ public/js/datepicker/datepicker_horizontal.js
@@ -1,17 +1,27 @@
 const date = new Date();
+const today = new Date();
 
 buildCalender();
 
 function buildCalender() {
-  let year = date.getFullYear();
-  let month = date.getMonth();
-  let day = date.getDay();
+  const year = date.getFullYear();
+  const month = date.getMonth();
 
-  let numbersOfCurrentMonthDates = new Date(year, month + 1, 0).getDate();
+  const todayMidnight = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate(),
+    0,
+    0,
+    0
+  );
+  const numbersOfCurrentMonthDates = new Date(year, month + 1, 0).getDate();
 
   let dates = "";
   for (let i = 1; i <= numbersOfCurrentMonthDates; i++) {
-    if (new Date(year, month, i).getDay() == 0) {
+    if (todayMidnight > new Date(year, month, i)) {
+      dates += `<div class="exdate">${i}</div>`;
+    } else if (new Date(year, month, i).getDay() == 0) {
       dates += `<div class="dateSun">${i}</div>`;
     } else if (new Date(year, month, i).getDay() == 6) {
       dates += `<div class="dateSat">${i}</div>`;
@@ -20,11 +30,48 @@ function buildCalender() {
     }
   }
 
+  const prevBtn = document.getElementById("prevBtn");
+  if (today >= date) {
+    prevBtn.disabled = true;
+  } else {
+    prevBtn.disabled = false;
+  }
+
   document.querySelector(".dates").innerHTML = dates;
-  document.querySelector(".selectedYear").innerHTML = `${date.getFullYear()}년`;
-  document.querySelector(".selectedMonth").innerHTML = `${
-    date.getMonth() + 1
-  }월`;
+  document.querySelector(".selectedYear").innerHTML = `${year}년`;
+  document.querySelector(".selectedMonth").innerHTML = `${month + 1}월`;
+
+  const dateHtmls = document.querySelectorAll(".dates > div");
+  for (let i = 0; i < dateHtmls.length; i++) {
+    dateHtmls[i].addEventListener("click", (e) => datesSelected(e));
+  }
+
+  function datesSelected(e) {
+    const convertedDate =
+      year + "." + (month + 1) + "." + e.target.innerText.split("\n")[0];
+    if (selectedDates.includes(convertedDate)) {
+      selectedDates = selectedDates.filter((item) => {
+        return item !== convertedDate;
+      });
+    } else {
+      if (selectedDates.length >= 2) {
+        clearSelectedDates();
+        clearSelectedDatesColor(dateHtmls);
+      }
+      selectedDates.push(convertedDate);
+      e.target.classList.add("dateClicked");
+    }
+
+    if (Date.parse(selectedDates[0]) > Date.parse(selectedDates[1])) {
+      validateAlert();
+      clearSelectedDates();
+      clearSelectedDatesColor(dateHtmls);
+    }
+
+    writeSelectedDates();
+    writeSumOfParkingFee();
+    colorDates();
+  }
 }
 
 function prevMonth() {
@@ -35,4 +82,48 @@ function prevMonth() {
 function nextMonth() {
   date.setMonth(date.getMonth() + 1);
   buildCalender();
+}
+
+function clearSelectedDates() {
+  selectedDates = [];
+}
+
+function clearSelectedDatesColor(dateHtmls) {
+  for (let i = 0; i < dateHtmls.length; i++) {
+    dateHtmls[i].classList.remove("dateClicked");
+  }
+}
+
+function colorDates() {
+  const dateHtmls = document.querySelectorAll(".dates > div");
+  const currentYear = document
+    .querySelector(".selectedYear")
+    .innerText.replace("년", "");
+  const currentMonth = document
+    .querySelector(".selectedMonth")
+    .innerText.replace("월", "");
+
+  const startYear = new Date(selectedDates[0]).getFullYear();
+  const startMonth = new Date(selectedDates[0]).getMonth();
+  const startDate = new Date(selectedDates[0]).getDate();
+  const endYear = new Date(selectedDates[1]).getFullYear();
+  const endMonth = new Date(selectedDates[1]).getMonth();
+  const endDate = new Date(selectedDates[1]).getDate();
+
+  if (
+    Date(currentYear, currentMonth) === Date(startYear, startMonth) &&
+    Date(currentYear, currentMonth) === Date(endYear, endMonth)
+  ) {
+    for (let i = 0; i < dateHtmls.length; i++) {
+      if (i >= startDate && i <= endDate) {
+        dateHtmls[i - 1].classList.add("dateClicked");
+      }
+    }
+  } else if (Date(currentYear, currentMonth) < Date(endYear, endMonth)) {
+    for (let i = 0; i < dateHtmls.length; i++) {
+      if (i >= startDate) {
+        dateHtmls[i - 1].classList.add("dateClicked");
+      }
+    }
+  }
 }
